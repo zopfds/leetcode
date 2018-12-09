d9a0064d2ad8d9d14930ad56b9137e057e6630f8
zopfds <740779066@qq.com>
2018-12-09 15:57:02 +0800

    leetcode下一个最大数的排列

--- src/main/java/leetcode/NextPermutation.java
+++ src/main/java/leetcode/NextPermutation.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import leetcode.util.Println;
+
 /**
  * 下一个排列
  *
@@ -9,93 +11,50 @@ package leetcode;
  */
 public class NextPermutation {
     public static void nextPermutation(int[] nums) {
-        int k;
         if(nums.length <= 1){
             return;
-        }else if(nums.length == 2){
-            k = nums[0];
-            nums[0] = nums[1];
-            nums[1] = k;
         }
 
-        for(int i = nums.length - 2 ; i >= 1 ; i--){
-            // 第一个大于第二个 423 ， 432 ， 433
-            if(nums[i - 1] > nums[i]){
-                //第二个大于第三个 432
-                if(nums[i] > nums[i + 1] || nums[i] == nums[i + 1]){
-                    continue;
-                    //第二个小于第三个 423,324,323
-                }else if(nums[i] < nums[i + 1]){
-                    k = nums[i];
-                    nums[i] = nums[i + 1];
-                    nums[i + 1] = k;
-                    return;
-                }
-                //第一个小于第二个 234，243，233, 342,232
-            }else if(nums[i - 1]< nums[i]){
-                //第二个大于第三个 243 342
-                if(nums[i] > nums[i + 1]){
-                    //第一个小于第三个 243
-                    if(nums[i-1] <  nums[i+1]){
-                        k = nums[i + 1];
-                        nums[i + 1] = nums[i];
-                        nums[i] = nums[i - 1];
-                        nums[i - 1] = k;
-                        return;
-                        //第一个大于第三个 342
-                    }else if(nums[i-1] > nums[i+1] || nums[i-1] == nums[i+1]){
-                        k = nums[i-1];
-                        nums[i-1] = nums[i];
-                        nums[i] = nums[i+1];
-                        nums[i+1] = k;
-                        return;
-                    }
-                    //第二个小于第三个 234
-                }else if(nums[i] < nums[i + 1]){
-                    k = nums[i];
-                    nums[i] = nums[i + 1];
-                    nums[i + 1] = k;
-                    return;
-                    //第二个等于第三个 233
-                }else{
-                    k =  nums[i - 1];
-                    nums[i - 1] = nums[i];
-                    nums[i] = k;
-                    return;
-                }
-                //第一个等于第二个 223 ， 221 , 222
-            }else{
-                //第二个大于第三个或第二个等于第三个,221,222
-                if(nums[i] == nums[i + 1] || nums[i] > nums[i + 1]){
-                    continue;
-                    //第二个小于第三个223
-                }else{
-                    k = nums[i];
-                    nums[i] = nums[i + 1];
-                    nums[i + 1] = k;
-                    return;
-                }
+        //先找出第一个nums[i] < nums[i + 1]的元素
+        int i = nums.length - 2;
+        for(; i >= 0 ; i--){
+            if(nums[i] < nums[i + 1]){
+                break;
             }
         }
 
-        for(int i = 0 ; i < (nums.length - 1) / 2 ; i ++){
+        int j = i + 1,k = 0;
+        //如果i在数组中存在，则进行下列判断
+        if(i != -1) {
+            //从i+1开始寻找比nums[i]大的数中最小的一个，令其下标为j
+            int biggerThani = nums[j];
+            for (int m = nums.length - 1 ; m > j; m--) {
+                if (nums[m] > nums[i] && nums[m] <= biggerThani) {
+                    biggerThani = nums[m];
+                    j = m;
+                    break;
+                }
+            }
+
+            //交换nums[i]和nums[j]
             k = nums[i];
-            nums[i] = nums[nums.length - 1 - i];
-            nums[nums.length - 1 -i] = k;
+            nums[i] = nums[j];
+            nums[j] = k;
+        }
+
+        //从j开始到数组末尾对称交换
+        for(int l = 0 ; l < (nums.length - (i + 1)) / 2 ; l ++){
+            k = nums[l + (i + 1)];
+            nums[l + (i + 1)] = nums[nums.length - 1 - l];
+            nums[nums.length - 1 - l] = k;
         }
 
         return;
     }
 
-    public boolean reverse(int[] nums , int i){
-        if(i <= nums.length - 2 && reverse(nums , i)){
-            return true;
-        }
-
-        if(nums[i] < nums[i + 1]){
-            
-        }
-
-        return reverse(nums , i + 1);
+    public static void main(String[] args){
+        int[] nums = {2,3,1,3,3};
+        nextPermutation(nums);
+        Println.printArray(nums);
     }
 }
